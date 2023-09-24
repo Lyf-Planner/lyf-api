10f3b88c5dd20811eadbd2fc9f592ba1e94abd90
Ethan Husband <ethan.husband@evup.com.au>
2023-09-24 13:05:22 +1000

    Remove script references to .env

--- src/index.ts
+++ src/index.ts
@@ -2,9 +2,12 @@ import { MongoClient, ServerApiVersion } from "mongodb";
 import { fetchSertUser, updateUser } from "./userOps";
 import { Request, Response } from "express";
 
+const dotenv = require('dotenv');
 const express = require("express");
 const server = express();
 const cors = require("cors");
+
+dotenv.config();
 
 // Create a MongoClient with a MongoClientOptions object to set the Stable API version
 const client = new MongoClient(process.env.MONGO_URL as string, {
