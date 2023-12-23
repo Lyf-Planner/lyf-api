1d9a092e3c1c29ff78a23a373661de1d09d2bd2f
Ethan Husband <ethan.husband@evup.com.au>
2023-12-23 16:29:28 +1100

    Merge prod patch

--- src/index.ts
+++ src/index.ts
@@ -6,6 +6,7 @@ import express from "express";
 import dotenv from "dotenv";
 import cors from "cors";
 import env from "./envManager";
+import bodyParserErrorHandler from "express-body-parser-error-handler";
 
 const server = express();
 
@@ -14,6 +15,7 @@ dotenv.config();
 //middleware
 server.use(cors());
 server.use(express.json());
+server.use(bodyParserErrorHandler());
 
 async function main() {
   server.get("/", (req: Request, res: Response) => {
