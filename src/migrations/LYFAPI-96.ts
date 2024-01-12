fb83e3ea12c6b696d094d66760594255492ddd78
Ethan Husband <ethan.husband@evup.com.au>
2024-01-12 13:49:42 +1100

    LYFAPI-96: Apply to all collections

--- src/migrations/LYFAPI-96.ts
+++ src/migrations/LYFAPI-96.ts
@@ -1,12 +1,11 @@
 import { MongoClient, ServerApiVersion } from "mongodb";
 import env from "../envManager";
 
-// https://lyf-planner.atlassian.net/browse/LYFAPI-23
+// https://lyf-planner.atlassian.net/browse/LYFAPI-96
 // Migrate timestamps from UTC format to ISO format
 
 export const migrate = async () => {
-  if (env.version !== "1.0.0") return;
-  console.log("Starting migration to API 2.0 Schema");
+  console.log("Starting migration LYFAPI-96");
   const client = new MongoClient(env.mongoUrl as string, {
     serverApi: {
       version: ServerApiVersion.v1,
@@ -18,16 +17,35 @@ export const migrate = async () => {
 
   var lyf_db = mongoConnection.db(env.mongoDb);
 
-  const allUsers = await lyf_db
-    .collection("users")
-    .find({ user_id: "etho" })
-    .toArray();
+    const allUsers = await lyf_db.collection("users").find().toArray();
+    console.log("\nMigrating users");
+    for (let user of allUsers) {
+      migrateItemInCollection(user, "users", lyf_db);
+    }
 
-  for (let user of allUsers) {
-    var last_updated = new Date(user.last_updated).toISOString();
-    var created = new Date(user.created).toISOString();
-    await lyf_db
-      .collection("users")
-      .updateOne({ _id: user._id }, { $set: { created, last_updated } });
+    const allItems = await lyf_db.collection("items").find().toArray();
+    console.log("\nMigrating items");
+    for (let item of allItems) {
+      await migrateItemInCollection(item, "items", lyf_db);
+    }
+
+  const allNotes = await lyf_db.collection("notes").find().toArray();
+  console.log("\nMigrating notes");
+  for (let note of allNotes) {
+    await migrateItemInCollection(note, "notes", lyf_db);
   }
+};
+
+const migrateItemInCollection = async (
+  item: any,
+  collection_name: string,
+  lyf_db: any
+) => {
+  console.log("\tMigrating", collection_name, item._id);
+  var last_updated = new Date(item.last_updated).toISOString();
+  var created = new Date(item.created).toISOString();
+  console.log("\tSetting created to", created, "last_updated to", last_updated);
+  await lyf_db
+    .collection(collection_name)
+    .updateOne({ _id: item._id }, { $set: { created, last_updated } });
 };
