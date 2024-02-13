a978e9f70f58ee8e46b10e5bba500f11e474b471
Ethan Husband <eth.husband@gmail.com>
2024-02-13 13:32:03 +1100

    LYFAPI-200: Friend notifications are async

--- src/models/social/friendNotifications.ts
+++ src/models/social/friendNotifications.ts
@@ -2,19 +2,19 @@ import { SocialUser } from "./socialUser";
 import expoPushService from "../../notifications/expoPushService";
 
 export class FriendNotifications {
-  public static newFriendRequest(to: SocialUser, from: SocialUser) {
+  public static async newFriendRequest(to: SocialUser, from: SocialUser) {
     let message = {
       to: to.getContent().expo_tokens || [],
       title: `${from.getContent().id} sent you a friend request`,
     };
-    expoPushService.pushNotificationToExpo([message]);
+    await expoPushService.pushNotificationToExpo([message]);
   }
 
-  public static newFriend(to: SocialUser, from: SocialUser) {
+  public static async newFriend(to: SocialUser, from: SocialUser) {
     let message = {
       to: to.getContent().expo_tokens || [],
       title: `${from.getContent().id} added you as a friend`,
     };
-    expoPushService.pushNotificationToExpo([message]);
+    await expoPushService.pushNotificationToExpo([message]);
   }
 }
