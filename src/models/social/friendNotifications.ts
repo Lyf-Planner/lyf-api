fd7fadf92d876ef5e1720c052e23ed27bc66bab9
Ethan Husband <eth.husband@gmail.com>
2024-02-14 11:37:48 +1100

    LYFAPI-148: Notifications for time/date changes + logging

--- src/models/social/friendNotifications.ts
+++ src/models/social/friendNotifications.ts
@@ -1,8 +1,15 @@
 import { SocialUser } from "./socialUser";
 import expoPushService from "../../notifications/expoPushService";
+import { Logger } from "../../utils/logging";
 
 export class FriendNotifications {
   public static async newFriendRequest(to: SocialUser, from: SocialUser) {
+    logger.info(
+      `Notifying ${to.getContent().id} of friend request from ${
+        from.getContent().id
+      }`
+    );
+
     let message = {
       to: to.getContent().expo_tokens || [],
       title: `${from.getContent().id} sent you a friend request`,
@@ -11,6 +18,12 @@ export class FriendNotifications {
   }
 
   public static async newFriend(to: SocialUser, from: SocialUser) {
+    logger.info(
+      `Notifying ${to.getContent().id} of accepted friend request from ${
+        from.getContent().id
+      }`
+    );
+
     let message = {
       to: to.getContent().expo_tokens || [],
       title: `${from.getContent().id} added you as a friend`,
@@ -18,3 +31,5 @@ export class FriendNotifications {
     await expoPushService.pushNotificationToExpo([message]);
   }
 }
+
+const logger = Logger.of(FriendNotifications);
--- src/models/social/socialItemNotifications.ts
+++ src/models/social/socialItemNotifications.ts
@@ -4,6 +4,11 @@ import { SocialUser } from "./socialUser";
 import { TwentyFourHourToAMPM, formatDate } from "../../utils/dates";
 import { UserOperations } from "../users/userOperations";
 import expoPushService from "../../notifications/expoPushService";
+import { ID } from "../../api/abstract";
+import { ItemModel } from "../items/itemModel";
+import { UserAccess } from "../../api/social";
+import { ListItem } from "../../api/list";
+import { Logger } from "../../utils/logging";
 
 export class SocialItemNotifications {
   public static async newItemInvite(
@@ -11,6 +16,11 @@ export class SocialItemNotifications {
     from: SocialUser,
     item: SocialItem
   ) {
+    logger.info(
+      `Notifying user ${
+        to.getContent().id
+      } of invite to item ${item.displayName()}`
+    );
     const itemContent = item.getContent();
 
     // Format the message
@@ -32,18 +42,21 @@ export class SocialItemNotifications {
   }
 
   public static async newItemUser(from: SocialUser, item: SocialItem) {
+    logger.info(
+      `Notifying users on item ${item.displayName()} of new user ${
+        from.getContent().id
+      }`
+    );
+
     // Notify all users (except the one who joined)
     const itemContent = item.getContent();
-    let usersToNotify = itemContent.permitted_users
-      .map((x) => x.user_id)
-      .filter((x) => x !== from.getContent().id);
+    let usersToNotify = this.usersExceptFrom(
+      from.getContent().id,
+      itemContent.permitted_users
+    );
 
-    // Get all notified user push tokens
-    let tokens = [] as string[];
-    for (let user_id of usersToNotify) {
-      let user_tokens = await UserOperations.getUserPushTokens(user_id);
-      tokens.concat(user_tokens);
-    }
+    // Get tokens
+    let tokens = await this.groupUserTokens(usersToNotify);
 
     // Format the message
     let message = {
@@ -54,4 +67,71 @@ export class SocialItemNotifications {
     // Send
     await expoPushService.pushNotificationToExpo([message]);
   }
+
+  public static async dateChanged(from: ID, item: ListItem) {
+    const newDate = item.date;
+    if (!newDate) return;
+
+    logger.info(
+      `Notifying users on item ${item.title} (${item.id}) of date change to ${newDate} from ${from}`
+    );
+
+    let usersToNotify = this.usersExceptFrom(from, item.permitted_users);
+
+    // Get tokens
+    let tokens = await this.groupUserTokens(usersToNotify);
+
+    // Format the message
+    let message = {
+      to: tokens,
+      title: `${from} updated the date of ${
+        item.title
+      } to ${TwentyFourHourToAMPM(newDate)}`,
+    } as ExpoPushMessage;
+
+    // Send
+    await expoPushService.pushNotificationToExpo([message]);
+  }
+
+  public static async timeChanged(from: ID, item: ListItem) {
+    const newTime = item.time;
+    if (!newTime) return;
+    logger.info(
+      `Notifying users on item ${item.title} (${item.id}) of time change to ${newTime} from ${from}`
+    );
+
+    let usersToNotify = this.usersExceptFrom(from, item.permitted_users);
+
+    // Get tokens
+    let tokens = await this.groupUserTokens(usersToNotify);
+
+    // Format the message
+    let message = {
+      to: tokens,
+      title: `${from} updated the time of ${
+        item.title
+      } to ${TwentyFourHourToAMPM(newTime)}`,
+    } as ExpoPushMessage;
+
+    // Send
+    await expoPushService.pushNotificationToExpo([message]);
+  }
+
+  // Helpers
+  public static usersExceptFrom(from_id: ID, permitted_users: UserAccess[]) {
+    return permitted_users.map((x) => x.user_id).filter((x) => x !== from_id);
+  }
+
+  public static async groupUserTokens(usersToNotify: string[]) {
+    // Get all notified user push tokens
+    let tokens = [] as string[];
+    for (let user_id of usersToNotify) {
+      let user_tokens = await UserOperations.getUserPushTokens(user_id);
+      tokens.concat(user_tokens);
+    }
+
+    return tokens;
+  }
 }
+
+const logger = Logger.of(SocialItemNotifications);
