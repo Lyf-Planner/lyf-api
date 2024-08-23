a7ca5e9b9583f1680a45b555ddfba8140e4082a4
Ethan Husband <eth.husband@gmail.com>
2024-08-23 23:30:57 +1000

    API-317: Debug logging for weather calls

--- src/modules/weather/weather_service.ts
+++ src/modules/weather/weather_service.ts
@@ -6,6 +6,7 @@ import { allDatesBetween, daysDifferenceBetween, formatDateData } from "../../ut
 import { Coordinates } from "../../api/schema/util/weather";
 import { UserEntity } from "../../models/entity/user_entity";
 import moment from "moment-timezone";
+import { Logger } from "../../utils/logging";
 
 const weatherInterface = new OpenWeatherAPI({
   key: env.openWeatherApiKey,
@@ -24,6 +25,10 @@ export class WeatherService {
       ...historicDates.map((date) => weatherInterface.getHistory(date, { coordinates })),
       weatherInterface.getDailyForecast(limit, true, { coordinates })
     ])).flat()
+
+    if (relevantDates.length < limit) {
+      new Logger('WeatherService').error(`low number of results - ${({ relevantDates })}`)
+    }
 
     return relevantDates;
   }
