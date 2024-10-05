968d0df857c7a0ed3d97d34901ce0a7b3da9616c
Ethan Husband <eth.husband@gmail.com>
2024-10-05 12:16:54 +1000

    API-349: auto_day_finishing should default to true

--- src/db/migrations/20241002_001_add_auto_day_finishing_to_users.ts
+++ src/db/migrations/20241002_001_add_auto_day_finishing_to_users.ts
@@ -3,7 +3,7 @@ import { sql, Kysely } from 'kysely';
 export async function up(db: Kysely<any>): Promise<void> {
   await db.schema
     .alterTable('users')
-    .addColumn('auto_day_finishing', 'boolean')
+    .addColumn('auto_day_finishing', 'boolean', (col) => col.defaultTo(true))
     .execute();
 }
 
