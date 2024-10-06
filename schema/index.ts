c5614ba8ab8c80f07ab0c3dfbce6a719c1f9ca96
Ethan Husband <eth.husband@gmail.com>
2024-10-06 12:10:02 +1100

    REFACTOR: Delete moved submodule to init again

--- schema/index.ts
+++
@@ -1,8 +0,0 @@
-import { Item, ItemRelatedEntity } from './items';
-import { Note, NoteRelatedEntity } from './notes';
-import { ExposedUser, PublicUser, User, UserRelatedEntity } from './user';
-
-export type Relation = ItemRelatedEntity | NoteRelatedEntity | UserRelatedEntity;
-export type Entity = ExposedUser | User | PublicUser | Item | Note;
-
-export type Export = Entity | Relation;
--- schema/notifications.ts
+++
@@ -1,7 +0,0 @@
-import { NotificationDbObject } from './database/notifications';
-
-export interface NotificationRelations {}
-
-export interface Notification extends NotificationDbObject {
-  relations: Partial<NotificationRelations>;
-}
