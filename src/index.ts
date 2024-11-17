9bd146a4b6fa43b4b6a64d906a8da8cd477262be
Ethan Husband <eth.husband@gmail.com>
2024-11-17 13:55:02 +1100

    API-352: Make notice endpoint available

--- src/index.ts
+++ src/index.ts
@@ -15,6 +15,7 @@ import env from './envManager';
 import reminderService from './modules/notification_scheduling/reminder_service';
 import { Logger, LoggingLevel } from './utils/logging';
 import { seedLatest } from './db/pg/seed_manager';
+import { PublicEndpoints } from './controller/routes/public_routes';
 
 export const server = express();
 
@@ -41,6 +42,7 @@ server.set('trust proxy', 1 /* number of proxies between user and server (expres
 export const serverInitialised = new Promise(async (resolve, reject) => {
   try {
     // Initialise endpoints
+    new PublicEndpoints(server);
     new UserEndpoints(server);
     new ItemEndpoints(server);
     new NoteEndpoints(server);
